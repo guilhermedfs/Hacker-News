13a38a478e5f14294b1191e40028db9124c36bb2
Guilherme Daniel Fernandes da Silva <guilhermedfs11@gmail.com>
2021-12-16 10:03:06 -0300

    Initial Commit

---
+++ Hacker App/Hacker_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_AppApp.swift
+//  Hacker App
+//
+//  Created by Guilherme Fernandes - pessoal on 16/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
